c22e51ea8db5e9a32f49e5a5fac4c9affa8c1243
Goorob <orobalswalqah@gmail.com>
2019-09-25 12:28:09 +0300

    confirmm

--- script/script.js
+++ script/script.js
@@ -2,13 +2,21 @@ var cal = prompt('how much Calories your food have ?');
 var food ;
 var yourname = prompt('what is your name ?');
 var yourage = prompt('how old are you ?')
-
+var fine =confirm('are you feeling fine?')
+var output
 if (cal > 8000){
     food = 'your food is not healthy';
 }
 else if (cal < 8000){
     food = ' your food is healthy ';
 } else 
+if ( fine ){
+    output= 'have agood day '
+}else {
+    output='you have to visit adoctor '
+}
+    
+
 document.write( '<h3>' + yourname + '</h3>');
 document.write( '<h3>' + yourage + '</h3>');
 document.write( '<h3>' + food + '</h3>');
