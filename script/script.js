e86d2d4eda3e4c02af6f689b1a182867de866658
Goorob <orobalswalqah@gmail.com>
2019-09-25 22:02:36 +0300

    hhh

--- script/script.js
+++ script/script.js
@@ -1,23 +1,27 @@
-var cal = prompt('how much Calories your food have ?');
-var food ;
+
+
 var yourname = prompt('what is your name ?');
 var yourage = prompt('how old are you ?');
+var calories
+function calories() {
+    var cal = prompt('how much Calories your food have ?');
+  var food;
+  if (cal > 18) {
+      food  = 'your food is not healthy';
+  } else 
+    food = 'your food is healthy';
+    return food;
 
 var output
-if (cal > 8000){
-    food = 'your food is not healthy';
-}
-else if (cal < 8000){
-    food = ' your food is healthy ';
-} 
+
 var fine =confirm('are you feeling fine?');
 if ( fine==true ){
     output= 'have agood day ';
- } else {
+ } else 
     output='you have to visit adoctor ';
 }
 
 document.write( '<h3>' + yourname + '</h3>');
 document.write( '<h3>' + yourage + '</h3>');
 document.write( '<h3>' + food + '</h3>');
-document.write( '<h3>' + output + '</h3>');
+
