4947f01e07bddf3dc3d6e1a3b58ce4b3b1dddb62
Goorob <orobalswalqah@gmail.com>
2019-09-26 08:19:33 +0300

    funss

--- script/script.js
+++ script/script.js
@@ -1,34 +1,38 @@
 
-
+'use strit'
 var yourname = prompt('what is your name ?');
 
-var calories
+var calories;
 function calories () {
     var cal = prompt('how much Calories your food have ?');
   var food;
-  if (cal > 18) {
+  if (cal > 8000) {
       food  = 'your food is not healthy';
-  } else 
+  } else {
     food = 'your food is healthy';
+  }
     return food;
 }
-function yourage() {
+function age () {
     var yourage = prompt('how old are you ?');
-    if ( yourage >=45  ){
+    if ( yourage >=45  ) {
         yourage = '<img src="https://now.tufts.edu/sites/default/files/bodyimages/160516_myplate_older_adults_inside.jpg>';
-     } else 
-yourage = "take care about your healthy "
-    }
-var output
+     } else {
+yourage = "take care about your healthy " }
+     
+return yourage;
+}
 
-var fine =confirm('are you feeling fine?');
-if ( fine==true ){
-    output= 'have agood day ';
- } else 
-    output='you have to visit adoctor ';
+var output;
 
+var fine = confirm('are you feeling fine?');
+if ( fine == true ) {
+    output = ' have agood day ';
+ } else {
+    output =' you have to visit adoctor ';
 
+}
 document.write( '<h3>' + yourname + '</h3>');
-document.write( '<h3>' + yourage + '</h3>');
-document.write( '<h3>' + food + '</h3>');
+
+document.write( '<h3>' + output + '</h3>');
 
